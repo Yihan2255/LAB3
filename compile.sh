3b8306835f742a801711e41aa52a8dc8ff55e2f8
Yihan2255 <yihan2255@gmail.com>
2026-02-27 11:39:48 +0100

    e

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gcc main.c parsePGM.c -o computeHistogram
-./computeHistogram ../Data/heart.pgm ../Data/histogram_heart.txt 4
-python3 ../showHistogram.py ../Data/histogram_heart.txt
+gcc src/main.c src/parsePGM.c -o computeHistogram -pthread
+#./computeHistogram ../Data/heart.pgm ../Data/histogram_heart.txt 4
+#python3 ../showHistogram.py ../Data/histogram_heart.txt
 
 #gcc P3_sequential.c parsePGM.c -o computeHistogramSequential
 #./computeHistogramSequential ../Data/heart.pgm ../Data/histogram_heart.txt
