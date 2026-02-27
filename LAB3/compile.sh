e07a6dd2734651081360816c6de611f2a53074c8
Yihan2255 <yihan2255@gmail.com>
2026-02-27 11:36:00 +0100

    e

--- LAB3/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcc main.c parsePGM.c -o computeHistogram
-./computeHistogram ../Data/heart.pgm ../Data/histogram_heart.txt 4
-python3 ../showHistogram.py ../Data/histogram_heart.txt
-
-#gcc P3_sequential.c parsePGM.c -o computeHistogramSequential
-#./computeHistogramSequential ../Data/heart.pgm ../Data/histogram_heart.txt
-#python3 ../showHistogram.py ../Data/histogram_heart.txt
